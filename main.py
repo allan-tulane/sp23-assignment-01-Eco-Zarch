a98118ab489371fb35f9e3d8a8e5fd9806894ccc
Zachary Wiel <zachary@ecobux.org>
2023-02-23 21:32:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,30 @@ def foo(n):
 
 
 def longest_run(mylist, key):
-    ### TODO
-    pass
+    runs = []
+    for i in range(len(mylist)):
+        n = 0
+        while mylist[i+n] == key:
+                n += 1
+        runs += n
+    return max(runs)
+
+def longest_run(mylist, key):
+    maxlength = 0
+    currlength = 0
+    prev = None
+    for item in mylist:
+        if (item == key) and (prev != key):
+            currlength = 1
+        if (item == prev):
+            currlength+=1
+            if currlength > maxlength:
+                maxlength = currlength
+        prev = item
+
+    return maxlength
+
+
 
 
 class Result:
@@ -32,11 +54,27 @@ class Result:
     
     
 def longest_run_recursive(mylist, key):
-    ### TODO
-    pass
+    return helper_longest_run_recursive(mylist, key).longest_size
+
+
+def helper_longest_run_recursive(mylist, key):
+    if mylist.count(key) == 0:
+        return Result(0,0,0, False)
+    if mylist.count(key) == len(mylist):
+        return Result(len(mylist),len(mylist),len(mylist),mylist[0]==key)
+
+    left = helper_longest_run_recursive(mylist[:(len(mylist)//2)],key)
+    right = helper_longest_run_recursive(mylist[(len(mylist)//2):],key)
+    middle = left.right_size + right.left_size
+
+    if ((middle) > right.longest_size) and ((middle) > left.longest_size):
+        return Result(0,0,middle,True)
+    return left if left.longest_size > right.longest_size else right
 
 ## Feel free to add your own tests here.
 def test_longest_run():
     assert longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3
+    assert longest_run_recursive([2,12,12,8,12,0,12,0,12,1], 12) == 2
+    assert longest_run_recursive([2,12,0,0,12,0,12,0,12,1], 0) == 2
 
-
+print(test_longest_run())
